bb895600c3dc4ffd1fd09882b3e93ba226cc6ce9
Sergey <94781301+SergueiMoscow@users.noreply.github.com>
2023-06-22 00:50:44 +0300

    add README.md

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from flask import render_template
+import plotly.express as px
+
 import httpx
 
 import private
@@ -58,3 +60,22 @@ class Weather:
         response = requests.get(url)
         print(f'Forecast: {response.json()}')
         return response.json()
+
+    @staticmethod
+    def get_forecast_fig(forecast_from_server: dict):
+        forecast = forecast_from_server['list']
+        date_time = []
+        temp = []
+        # clouds = []
+        # humidity = []
+        # forecast_clean = []
+        for dt in forecast:
+            date_time.append(dt['dt_txt'])
+            temp.append(dt['main']['temp'])
+            # clouds.append(dt['clouds']['all'])
+            # forecast_clean.append({'dt': dt['dt_txt'], 'temp': dt['main']['temp'], 'humidity': dt['main']['humidity'], 'clouds': dt['clouds']['all']})
+            # print(dt)
+
+        # print(forecast_clean)
+        fig = px.line(x=date_time, y=temp)
+        return fig
